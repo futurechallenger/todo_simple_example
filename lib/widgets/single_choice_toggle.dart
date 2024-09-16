990a7b78e8cfe3694e1cdcbcdc573ca92cee978a
Uncle Charlie <uncle_charlie@maddog.com>
2024-09-16 18:43:38 +0800

    add switch-toggle

---
+++ lib/widgets/single_choice_toggle.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SingleChoiceToggle extends StatefulWidget {
+  const SingleChoiceToggle({super.key});
+
+  @override
+  State<SingleChoiceToggle> createState() => _SingleChoiceToggleState();
+}
+
+class _SingleChoiceToggleState extends State<SingleChoiceToggle> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<int>(
+      segments: const [
+        ButtonSegment(value: 0, label: Text('By ID')),
+        ButtonSegment(value: 1, label: Text('By Title')),
+      ],
+      selected: <int>{selectedIndex},
+      onSelectionChanged: (Set<int> newSelection) {
+        setState(() {
+          selectedIndex = newSelection.first;
+        });
+      },
+    );
+  }
+}
