512b2dc3bffa10a1435f989928e8d68760eb59aa
Uncle Charlie <uncle_charlie@maddog.com>
2024-09-19 22:45:45 +0800

    swap color tiles

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final List<Widget> tiles = [
+    Padding(
+      key: UniqueKey(),
+      padding: const EdgeInsets.all(8.0),
+      child: StatefulTile(
+        key: UniqueKey(),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: StatefulTile(
+        key: UniqueKey(),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +31,60 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: const Text('Settings'),
+      body: Row(
+        children: tiles,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.swap_calls),
+        onPressed: () {
+          debugPrint("floating button is clicked");
+          setState(() {
+            tiles.insert(1, tiles.removeAt(0));
+          });
+        },
+      ),
+    );
+  }
+}
+
+class StatelessTile extends StatelessWidget {
+  StatelessTile({super.key});
+
+  final Color theColor = Color(math.Random().nextInt(0xffffffff));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      color: theColor,
+    );
+  }
+}
+
+class StatefulTile extends StatefulWidget {
+  const StatefulTile({super.key});
+
+  @override
+  State<StatefulTile> createState() => _StatefulTileState();
+}
+
+class _StatefulTileState extends State<StatefulTile> {
+  late Color theColor;
+
+  @override
+  void initState() {
+    super.initState();
+
+    theColor = Color(math.Random().nextInt(0xffffffff));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      color: theColor,
     );
   }
 }
